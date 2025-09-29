1499448db6d42b958aa5e91caeb04762dff4d80b
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-29 09:10:57 -0500

    feat: initial commit for script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('start-ar').addEventListener('click', function() {
+    // Funktionalität zum Starten der Augmented Reality
+    alert('AR-Funktionalität noch nicht implementiert.');
+});
+
+// Beispielcode für interaktive Karte
+function initMap() {
+    const mapContainer = document.getElementById('map-container');
+    // Hier sollte der Code zur Initialisierung der Karte stehen.
+    mapContainer.innerHTML = 'Karte wird geladen...';
+}
+initMap();
